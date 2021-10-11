cd075d43edf10b58f283a35725ca5c33f40406a8
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-11 18:10:43 -0400

    added app.get

--- myApp.js
+++ myApp.js
@@ -3,10 +3,16 @@ var app = express();
 
 
 console.log("Hello World");
+app.get("/", (req, res) => {
+    
+    res.send("Hello Express");
+
+
+} );
 
 app.get("/", (req, res) => {
     
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 
 } );
 
