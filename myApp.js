ba7fe40c8fa29109269f9da1b56946218b1ffcc7
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-13 13:36:54 -0400

    added middleware

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-
-app.use('/json',function middleWare(req, res. next){
-    var string = req.method + " " + req.path + " - " + req.ip;
+app.use( (req, res, next)=>{
+    console.log("req.method+ ’ ‘+req.path+’-’+req.ip ");
     next();
-});
+    })
+
 
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase"){
