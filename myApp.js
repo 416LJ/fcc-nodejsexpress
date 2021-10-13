3cf966195625a60b546b71fcaa66a79514e16ca1
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-13 13:34:29 -0400

    added middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+
+app.use('/json',function middleWare(req, res. next){
+    var string = req.method + " " + req.path + " - " + req.ip;
+    next();
+});
+
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase"){
         res.json({
@@ -18,10 +24,7 @@ app.get("/json", (req, res) => {
 
 
 } );
-app.use('/json',function middleWare(req, res. next){
-    var string = req.method + " " + req.path + " - " + req.ip;
-    next();
-});
+
 app.use("/public",express.static(__dirname + "/public"));
 
 
