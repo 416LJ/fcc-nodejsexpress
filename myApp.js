3d1327a95bc21a868b6f8dacfcb09789df5527a6
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-13 11:03:52 -0400

    added .env message

--- myApp.js
+++ myApp.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get("/json", (req, res) => {
-    res.json({
-        "message" : "Hello json"
-    });
-    
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({
+            message: "Hello json"
+          .toUpperCase()});
+
+    }else{
+        res.json({
+            message: "Hello json"
+          });
+    }
 
 } );
 
