ecc14056c543992c489b59cdef362d932a283caf
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-13 13:47:43 -0400

    added middleware

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use((req, res, next)=>{
-    console.log(req.method + req.path + req.ip);
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 })
 
