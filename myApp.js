17e93339f44a06aaa842cc28d542faec807f27c9
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-13 11:12:25 -0400

    added .env message

--- myApp.js
+++ myApp.js
@@ -13,6 +13,10 @@ app.get("/json", (req, res) => {
           });
     }
 
+    res.sendFile(__dirname + "/views/index.html");
+    app.use("/public", express.static(__dirname + "/public"));
+
+
 } );
 
 app.use("/public",express.static(__dirname + "/public"));
