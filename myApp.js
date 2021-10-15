b6d7e3f683a0a61abf903fafdc7e233ccbae35b4
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-15 18:20:46 -0400

    added req params first and last name

--- myApp.js
+++ myApp.js
@@ -1,35 +1,34 @@
 const { json } = require('body-parser');
 var express = require('express');
 var app = express();
-// reusable middleware function
-const middleware = (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  };
-// chained middleware with reusable function
-  app.get("/now", middleware, (req, res) => {
-    res.send({
-      time: req.time
-    });
-  });
-
-//
 
 
-
+// 7. root level logger
 app.use((req, res, next)=>{
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
-})
+});
 
 
-// serve html file
+//1. meet the node console
+console.log("hello laxsan");
+
+//2. working express server
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+  });
+
+
+
+// 3. serve html file
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
                 
-  
-  // serve json on specific route with .env file
+// 4. serve static assets
+app.use("/public",express.static(__dirname + "/public"));
+
+// 5 and 6. serve json on specific route with .env file
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase"){
         res.json({
@@ -43,6 +42,21 @@ app.get("/json", (req, res) => {
     }
 } );
 
+// 8. create time server with reusable function
+// reusable middleware function
+const middleware = (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  };
+// chained middleware with reusable function
+  app.get("/now", middleware, (req, res) => {
+    res.send({
+      time: req.time
+    });
+  });
+
+// 9 . get route parameter from user
+
 app.get("/:word/echo", (req, res) => {
     const { word } = req.params;
     res.json({
@@ -50,11 +64,11 @@ app.get("/:word/echo", (req, res) => {
     })
 });
 
+// 10. get parameter query
+
 app.get("/name", (req, res) => {
   const first = req.query.first;
   const last= req.query.last;   
-
-
   res,json({
       name: `${first} ${last}`
 
@@ -63,8 +77,7 @@ app.get("/name", (req, res) => {
 
 
 
-//serve static assets
-app.use("/public",express.static(__dirname + "/public"));
+
 
 
 
